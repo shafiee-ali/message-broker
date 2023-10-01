1ac2b26f4f77d5a2379feaf92bdc9a76dcb8f5b6
Ali Shafiee <shafiee.ali10@gmail.com>
2023-10-01 15:44:17 +0330

    Create type for created message

---
+++ internal/types/message.go
@@ -0,0 +1,22 @@
+package types
+
+import (
+	"therealbroker/pkg/models"
+	"time"
+)
+
+type CreatedMessage struct {
+	Id             int
+	Subject        string
+	Body           string
+	ExpirationTime time.Time
+}
+
+func NewCreatedMessage(msgModel models.Message) CreatedMessage {
+	return CreatedMessage{
+		Id:             msgModel.Id,
+		Subject:        msgModel.Subject,
+		Body:           msgModel.Body,
+		ExpirationTime: msgModel.ExpirationTime,
+	}
+}
