fa5230e7e3bdd57838f0676b748d0afd13df9dd6
Ali Shafiee <shafiee.ali10@gmail.com>
2023-10-01 16:07:50 +0330

    Return error if server is closed

--- internal/broker/module.go
+++ internal/broker/module.go
@@ -2,12 +2,15 @@ package broker
 
 import (
 	"context"
+	"sync"
 	"therealbroker/pkg/broker"
 	"therealbroker/pkg/repository"
 )
 
 type Module struct {
 	repository repository.IMessageRepository
+	lock       sync.Mutex
+	isClosed   bool
 }
 
 func NewModule(repo repository.IMessageRepository) broker.Broker {
@@ -15,10 +18,20 @@ func NewModule(repo repository.IMessageRepository) broker.Broker {
 }
 
 func (m *Module) Close() error {
-	panic("implement me")
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if m.isClosed {
+		return broker.ErrUnavailable
+	}
+	m.isClosed = true
+	return nil
 }
 
 func (m *Module) Publish(ctx context.Context, msg broker.CreateMessageDTO) (int, error) {
+	err := m.checkServerDown()
+	if err != nil {
+		return -1, err
+	}
 	createdMessage := m.repository.Add(msg)
 	return createdMessage.Id, nil
 }
@@ -29,4 +42,13 @@ func (m *Module) Subscribe(ctx context.Context, subject string) (<-chan broker.C
 
 func (m *Module) Fetch(ctx context.Context, subject string, id int) (broker.CreateMessageDTO, error) {
 	panic("implement me")
+}
+
+func (m *Module) checkServerDown() error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+	if m.isClosed {
+		return broker.ErrUnavailable
+	}
+	return nil
 }
