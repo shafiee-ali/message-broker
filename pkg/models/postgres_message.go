cb4cf9341d090e5eedd75228c40b02267c88d3ba
Ali Shafiee <shafiee.ali10@gmail.com>
2023-10-05 04:15:50 +0330

    Create postgres message model and repo

---
+++ pkg/models/postgres_message.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"fmt"
+	"time"
+)
+
+type PostgresMessage struct {
+	ID             int       `gorm:"primary_key"`
+	Subject        string    `gorm:"not null"`
+	Body           string    `gorm:"not null"`
+	ExpirationTime time.Time `gorm:"not null"`
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+func (msg *PostgresMessage) IsExpired() bool {
+	x := time.Now()
+	y := msg.ExpirationTime
+	fmt.Println(x, y)
+	return msg.ExpirationTime.Before(time.Now())
+}
---
+++ pkg/repository/postgres_message_repo.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"github.com/sirupsen/logrus"
+	"sync"
+	"therealbroker/internal/mapper"
+	"therealbroker/internal/types"
+	pkgBroker "therealbroker/pkg/broker"
+	"therealbroker/pkg/database"
+	"therealbroker/pkg/models"
+)
+
+type PostgresRepo struct {
+	db              *database.PostgresDB
+	insertLock      sync.Mutex
+	messageId       int
+	incrementIdLock sync.Mutex
+}
+
+func NewPostgresRepo(db *database.PostgresDB) PostgresRepo {
+	return PostgresRepo{
+		db:        db,
+		messageId: 0,
+	}
+}
+
+func (p *PostgresRepo) Add(message pkgBroker.CreateMessageDTO) types.CreatedMessage {
+	dbMsg := mapper.CreateMessageDTOToDBMessage(message)
+
+	//p.incrementIdLock.Lock()
+	//dbMsg.ID = p.messageId
+	//p.messageId++
+	//p.incrementIdLock.Unlock()
+
+	p.insertLock.Lock()
+	p.db.DB.Create(&dbMsg)
+	logrus.Debugf("Created record %v", dbMsg)
+	p.insertLock.Unlock()
+	return mapper.DBMessageToCreatedMessage(dbMsg)
+}
+
+func (p *PostgresRepo) FetchUnexpiredBySubjectAndId(subject string, id int) (types.CreatedMessage, error) {
+	var message models.PostgresMessage
+	p.db.DB.Where("subject = ? AND id = ?", subject, id).First(&message)
+	if message.IsExpired() {
+		return types.CreatedMessage{}, pkgBroker.ErrExpiredID
+	}
+	return mapper.DBMessageToCreatedMessage(message), nil
+}
