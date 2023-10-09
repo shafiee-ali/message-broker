a1288704710e7d6d36b31a28a5cb98d3f6b65d83
Ali Shafiee <shafiee.ali10@gmail.com>
2023-10-09 13:21:16 +0330

    Add enums for metrics

---
+++ pkg/metrics/enums.go
@@ -0,0 +1,12 @@
+package metrics
+
+const (
+	PUBLISH_METHOD   = "PUBLISH"
+	SUBSCRIBE_METHOD = "SUBSCRIBE"
+	FETCH_METHOD     = "FETCH"
+)
+
+const (
+	SUCCESS_STATUS = "SUCCESS"
+	ERROR_STATUS   = "ERROR"
+)
