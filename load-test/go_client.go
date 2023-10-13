a454b043eddffa765d49b68561f57c7991ac978e
Ali Shafiee <shafiee.ali10@gmail.com>
2023-10-13 23:30:56 +0330

    Change load test file

--- load-test/go_client.go
+++ load-test/go_client.go
@@ -32,23 +32,26 @@ func main() {
 	const CONCURRENT_PUBLISH_COUNT = 100000
 
 	wg := sync.WaitGroup{}
-	for i := 0; i < CONCURRENT_PUBLISH_COUNT; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			response, err := client.Publish(
-				context.Background(),
-				&pb.PublishRequest{
-					Subject:           randomString(2),
-					Body:              []byte(randomString(20)),
-					ExpirationSeconds: 1000,
-				})
-			if err != nil {
-				fmt.Printf("Error in publish %v \n", err)
-			} else {
-				fmt.Printf("Success %v\n", response)
-			}
-		}()
+	for j := 0; j < 10; j++ {
+		for i := 0; i < CONCURRENT_PUBLISH_COUNT; i++ {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				response, err := client.Publish(
+					context.Background(),
+					&pb.PublishRequest{
+						Subject:           randomString(2),
+						Body:              []byte(randomString(20)),
+						ExpirationSeconds: 1000,
+					})
+				if err != nil {
+					fmt.Printf("Error in publish %v \n", err)
+				} else {
+					fmt.Printf("Success %v\n", response)
+				}
+			}()
+		}
+		wg.Wait()
 	}
-	wg.Wait()
+
 }
